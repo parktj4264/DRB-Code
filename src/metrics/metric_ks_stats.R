1eade676143df48fbaff50b33b9bb1b19facd1c2
Taejun Park <123566749+parktj4264@users.noreply.github.com>
2026-05-11 00:07:09 +0900

    Split KS metrics into dedicated metric_*.R files

---
+++ src/metrics/metric_ks_stats.R
@@ -0,0 +1,19 @@
+metric_ks_stats <- function(pair_stats, raw_access) {
+  score <- vapply(seq_len(nrow(pair_stats)), function(i) {
+    msr <- as.character(pair_stats$MSR[i])
+    ref_group <- as.character(pair_stats$ref_group[i])
+    target_group <- as.character(pair_stats$target_group[i])
+
+    raw_pair <- raw_access$get_pair(msr, ref_group, target_group)
+    ref_values <- as.numeric(raw_pair$ref_values)
+    tgt_values <- as.numeric(raw_pair$tgt_values)
+
+    if (length(ref_values) == 0 || length(tgt_values) == 0) {
+      return(NA_real_)
+    }
+
+    as.numeric(suppressWarnings(stats::ks.test(tgt_values, ref_values)$statistic))
+  }, numeric(1))
+
+  as.numeric(score)
+}
