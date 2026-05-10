159051d36014391451648600ac069efe1f0f8f35
Taejun Park <123566749+parktj4264@users.noreply.github.com>
2026-05-11 00:20:31 +0900

    Rename AD helper function symbols and references

--- src/metrics/metric_ad_stats.R
+++ src/metrics/metric_ad_stats.R
@@ -2,20 +2,20 @@
 #
 # Formula (per MSR pair):
 #   score = AD statistic from twosamples::ad_test(target_raw, reference_raw)
-# Uses cached AD results from metric_ad_shared.R.
+# Uses cached AD results from helper_function_ad.R.
 metric_ad_stats <- function(pair_stats, raw_access) {
   if (!requireNamespace("twosamples", quietly = TRUE)) {
     return(rep(NA_real_, nrow(pair_stats)))
   }
 
-  nboots <- get_ad_nboots()
+  nboots <- helper_function_get_ad_nboots()
 
   score <- vapply(seq_len(nrow(pair_stats)), function(i) {
     msr <- as.character(pair_stats$MSR[i])
     ref_group <- as.character(pair_stats$ref_group[i])
     target_group <- as.character(pair_stats$target_group[i])
 
-    ad_pair <- get_ad_pair_result(raw_access, msr, ref_group, target_group, nboots)
+    ad_pair <- helper_function_get_ad_pair_result(raw_access, msr, ref_group, target_group, nboots)
     as.numeric(ad_pair[[1]])
   }, numeric(1))
 
